ab5bfa3f5ee2c673bcaaf52e91b0acb9d3b59551
Salvador De la Tijera <106124460+SalvadorDelaTijera@users.noreply.github.com>
2024-02-26 12:03:08 -0600

    nuevos atributos tailwind

--- src/Data/Data.jsx
+++ src/Data/Data.jsx
@@ -5,19 +5,19 @@ import React from "react"
 
 const Data = ({imagen, id, name, gender, specie, origen, location, status}) => {
   return (
-    <div className=" bg-gray-50 " >
-      <div className="md:flerounded-xl  dark:bg-slate-800 font-mono text-center">
-        <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto p-2 m-5git " src={imagen} alt="" />
-        <h5 className=""> Id: {id}</h5>
-        <h3 className="text-lg font-medium">{name}</h3>
-        <p>{gender}</p>
-        <p>{specie}</p>
-        <p>{origen}</p>
-        <p>{location}</p>
-        <p>{status}</p>
+    <div className="">
+      <div className="md:flerounded-xl  dark:bg-slate-800 font-mono text-center w-fit h-fit p-6 m-20 rounded-t-3xl rounded-b-3xl border-black" >
+        <img className="md:w-80 md:h-auto md:rounded-none mx-auto bg-contain w-fit h-fit items-center" src={imagen} alt="" />
+        <div className=" bg-black p10  justify-center bg-cover w-45 h-50 fit-text">
+        <h5 className=" text-yellow-300"> Id: {id}</h5>
+            <h3 className="text-lg font-medium text-red-900">{name}</h3>
+            <p className="text-md font-thin text-white">{gender}</p>
+            <p className="text-md font-thin text-white">{specie}</p>
+            <p className="text-md font-thin text-white">{origen}</p>
+            <p className="text-md font-thin text-white">{location}</p>
+            <p className="text-md font-thin text-white">{status}</p>
+        </div>
       </div>
- 
-
     </div>
   )
 }
